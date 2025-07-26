3e2dac7bb5ce0f5fce2b326a5772b16e613a73f4
Starry <starry00wolf@gmail.com>
2025-07-26 19:05:58 -0400

    wrong name lol

--- Formula/menuicon.rb
+++ Formula/menuicon.rb
@@ -1,7 +1,7 @@
 class Menuicon < Formula
   desc "Display multiple icons/letters in macOS menu bar using Python"
-  homepage "https://github.com/starry/menuicon"
-  url "https://github.com/starry/menuicon/archive/refs/tags/v1.0.1.tar.gz"
+  homepage "https://github.com/Starry0Wolf/menuicon"
+  url "https://github.com/Starry0Wolf/menuicon/archive/refs/tags/v1.0.1.tar.gz"
   sha256 "5e8366fe7a27b0bb60a05f940892cd1846b87ed9bd4d0be6f57d0c9f5f9cc5c9"
   license "MIT"
   version "1.0.1"
