d979583f335c8f1806ef3021373fe5c4e4a2b7b1
Starry <starry00wolf@gmail.com>
2025-07-28 20:35:12 -0400

    🔪work

--- Formula/menuicon.rb
+++ Formula/menuicon.rb
@@ -1,20 +1,29 @@
 class Menuicon < Formula
+  include Language::Python::Virtualenv
+
   desc "Display reversed strings as individual menu bar icons"
   homepage "https://github.com/Starry0Wolf/menuicon"
-  url "https://github.com/Starry0Wolf/menuicon/archive/refs/tags/v1.0.6.tar.gz"
-  sha256 "dae66e38ba7cc2eeddfad00d567f08d8c1f829666122e56b86ef9fc475aae19b"
-  license "MIT"
+  url   "https://github.com/Starry0Wolf/menuicon/archive/refs/tags/v1.0.7.tar.gz"
+  sha256 "9394676b60b954fdebf6ec5cf5c27ea84d3534aa6651ef2decd95825f5983062"
 
   depends_on "python@3.11"
 
+  # === Python deps ===
+  resource "pyobjc-core" do
+    url "https://files.pythonhosted.org/…/pyobjc-core-11.1.tar.gz"
+    sha256 "b63d4d90c5df7e762f34739b39cc55bc63dbcf9fb2fb3f2671e528488c7a87fe"
+  end
+
+  resource "pyobjc-framework-Cocoa" do
+    url "https://files.pythonhosted.org/…/pyobjc-framework-Cocoa-11.1.tar.gz"
+    sha256 "87df76b9b73e7ca699a828ff112564b59251bb9bbe72e610e670a4dc9940d038"
+  end
+
   def install
-    # Install your Python script directly as an executable
-    bin.install "menuicon.py" => "menuicon"
-    chmod 0755, bin/"menuicon"
+    virtualenv_install_with_resources
   end
 
   test do
-    # It should at least print its help/usage text
-    assert_match "Display reversed string", shell_output("#{bin}/menuicon --help", 1)
+    assert_match "Display reversed strings", shell_output("#{bin}/menuicon --help")
   end
 end
