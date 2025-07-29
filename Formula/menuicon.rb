ef2f29612259e50ea653d81527a93c9550410b24
Starry <starry00wolf@gmail.com>
2025-07-28 20:41:06 -0400

    hhhhh

--- Formula/menuicon.rb
+++ Formula/menuicon.rb
@@ -3,19 +3,19 @@ class Menuicon < Formula
 
   desc "Display reversed strings as individual menu bar icons"
   homepage "https://github.com/Starry0Wolf/menuicon"
-  url   "https://github.com/Starry0Wolf/menuicon/archive/refs/tags/v1.0.7.tar.gz"
-  sha256 "9394676b60b954fdebf6ec5cf5c27ea84d3534aa6651ef2decd95825f5983062"
+  url   "https://github.com/Starry0Wolf/menuicon/archive/refs/tags/v1.0.8.tar.gz"
+  sha256 "fb10439b4a71005804eb12ce8a3455dab2d134b4bc5d3cca32bbfe6a7565cbe7"
 
   depends_on "python@3.11"
 
   # === Python deps ===
   resource "pyobjc-core" do
-    url "https://files.pythonhosted.org/…/pyobjc-core-11.1.tar.gz"
-    sha256 "b63d4d90c5df7e762f34739b39cc55bc63dbcf9fb2fb3f2671e528488c7a87fe"
+  url "https://files.pythonhosted.org/packages/source/p/pyobjc-core/pyobjc-core-11.1.tar.gz"
+  sha256 "b63d4d90c5df7e762f34739b39cc55bc63dbcf9fb2fb3f2671e528488c7a87fe"
   end
 
   resource "pyobjc-framework-Cocoa" do
-    url "https://files.pythonhosted.org/…/pyobjc-framework-Cocoa-11.1.tar.gz"
+    url "https://files.pythonhosted.org/packages/source/p/pyobjc-framework-Cocoa/pyobjc-framework-Cocoa-11.1.tar.gz"
     sha256 "87df76b9b73e7ca699a828ff112564b59251bb9bbe72e610e670a4dc9940d038"
   end
 
