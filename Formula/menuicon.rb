1bad6737f6e2802e40bd782b13aca1ada5fbc431
Starry <starry00wolf@gmail.com>
2025-07-28 21:07:07 -0400

    aaaaa

--- Formula/menuicon.rb
+++ Formula/menuicon.rb
@@ -4,7 +4,7 @@ class Menuicon < Formula
   desc "Display reversed strings as individual menu bar icons"
   homepage "https://github.com/Starry0Wolf/menuicon"
   url   "https://github.com/Starry0Wolf/menuicon/archive/refs/tags/v1.0.10.tar.gz"
-  sha256 "da3a58cf82862d0bbd05eac08ab13a73d250834c042f03ea50578a171244d445"
+  sha256 "<UPDATE_SHA256_FOR_v1.0.10>"
   license "MIT"
   revision 1
 
@@ -21,10 +21,21 @@ class Menuicon < Formula
     sha256 "87df76b9b73e7ca699a828ff112564b59251bb9bbe72e610e670a4dc9940d038"
   end
 
-  # Patch in a minimal setup.py so pip can install the project
-  patch :DATA
-
   def install
+    # Inject a minimal setup.py if one is not present upstream
+    (buildpath/"setup.py").write <<~EOS
+      from setuptools import setup
+
+      setup(
+          name="menuicon",
+          version="1.0.10",
+          py_modules=["menuicon"],
+          entry_points={
+              "console_scripts": ["menuicon=menuicon:main"],
+          },
+      )
+    EOS
+
     virtualenv_install_with_resources
   end
 
@@ -32,18 +43,3 @@ class Menuicon < Formula
     assert_match "Display reversed strings", shell_output("#{bin}/menuicon --help")
   end
 end
-
-__END__
-*** Begin Patch
-*** Add File: setup.py
-+from setuptools import setup
-+
-+setup(
-+    name="menuicon",
-+    version="1.0.8",
-+    py_modules=["menuicon"],
-+    entry_points={
-+        "console_scripts": ["menuicon=menuicon:main"],
-+    },
-+)
-*** End Patch
